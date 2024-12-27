b7bebe0fbaaf23fcdc075424b26f24f2f4916867
lyj00912 <3122742760@qq.com>
2024-12-27 20:45:15 +0800

    created picture.DrawZoom(), also updated the docs.

--- Magic.h/Magic.h
+++ Magic.h/Magic.h
@@ -504,6 +504,48 @@ public:
 	}
 
 	/**
+	 * @brief: Draw this picture in the position (x, y) on the screen,
+	 *         With a zooming ratio in range [0.0f, 1.0f]
+	 *         to control how big the image should be drawn.
+	 * @note: The zooming center: Left-Up Corner of this picture.
+	 * @param x_: The x coordinate of the drawing position.
+	 * @param y_: The x coordinate of the drawing position.
+	 * @param ratio: The zooming ratio, in range [0.0f, 1.0f].
+	 * @return void
+	 */
+
+	void DrawZoom(int x_, int y_, float ratio) {
+
+		int screenStartX = x_;
+		int screenStartY = y_;
+		int screenEndX = x_ + int(this->width * ratio);
+		int screenEndY = y_ + int(this->height * ratio);
+
+		screenStartX = clamp(0, screenStartX, G_SCREEN_WIDTH);
+		screenStartY = clamp(0, screenStartY, G_SCREEN_HEIGHT);
+		screenEndX = clamp(0, screenEndX, G_SCREEN_WIDTH);
+		screenEndY = clamp(0, screenEndY, G_SCREEN_HEIGHT);
+
+		unsigned char ri, gi, bi;
+
+		for (int y = screenStartY; y < screenEndY; y++) {
+			for (int x = screenStartX; x < screenEndX; x++) {
+
+				ri = this->GetR(int((x - x_) / ratio), int((y - y_) / ratio));
+				gi = this->GetG(int((x - x_) / ratio), int((y - y_) / ratio));
+				bi = this->GetB(int((x - x_) / ratio), int((y - y_) / ratio));
+
+				if (ri == 255 && gi == 0 && bi == 255) {
+					// Pink, Transparent, Do Not Draw.
+					continue;
+				}
+
+				MagicSetPixel(x, y, ri, gi, bi);
+			}
+		}
+	}
+
+	/**
 	 * @brief: Set the color of a specific pixel IN THIS PICTURE.
 	 * @param x: The x coordinate of the specific pixel.
 	 * @param y: The y coordinate of the specific pixel.
