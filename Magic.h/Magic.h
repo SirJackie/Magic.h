d653964b767c9f181e8de0c27275b66a3471a8d4
lyj00912 <3122742760@qq.com>
2024-12-27 21:08:14 +0800

    added project description and license information in Magic.h.

--- Magic.h/Magic.h
+++ Magic.h/Magic.h
@@ -1,3 +1,29 @@
+/*
+ * The Magic Graphical Interface
+ * Author: SirJackie
+ * Project Repository: https://github.com/SirJackie/Magic.h
+ * Project Repository: https://gitee.com/SirJackie/Magic.h
+ *
+ * Description:
+ * A simple and elegant graphics programming framework that allows you
+ * to create a window with just one line of code.
+ *
+ * License:
+ * This software is licensed under the Mulan PSL v2 License.
+ * You may use, copy, modify, and distribute this software
+ * for any purpose with or without fee, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
+ * FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+ * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ */
+
 #ifndef __Magic_H__
 #define __Magic_H__
 
